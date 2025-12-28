f76e1eceb26fd2c8d035a3d2e359c123885d82b8
ibadah-counter <ibadahcounter@gmail.com>
2025-12-28 11:07:28 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE="ibadah-counter-v1";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js"];
+
+self.addEventListener("install",e=>{
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+ self.skipWaiting();
+});
+
+self.addEventListener("activate",e=>{
+ e.waitUntil,
+ self.clients.claim();
+});
+
+self.addEventListener("fetch",e=>{
+ e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+ );
+});
